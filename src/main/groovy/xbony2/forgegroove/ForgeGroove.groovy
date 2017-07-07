9cd7ed6a534e25d5a00170a1fb05563e5fb8f545
Eric Schneider <xbony22@gmail.com>
2017-07-07 11:52:25 -0400

    Having some pains with git/gradle but there.
    This compiles, this runs. This doesn't build (well, it does, but not
    with Groovy packaged). More far than I got with golo so yeah
    
    (is currently just a groovy port of the examplemod)

---
+++ src/main/groovy/xbony2/forgegroove/ForgeGroove.groovy
@@ -0,0 +1,17 @@
+package xbony2.forgegroove
+
+import net.minecraft.init.Blocks
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+
+@Mod(modid = ForgeGroove.MODID, version = ForgeGroove.VERSION)
+class ForgeGroove {
+	public static final String MODID = "forgegroove"
+	public static final String VERSION = "1.0.0a"
+	
+	@EventHandler
+	void init(FMLInitializationEvent event){
+		System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
+	}
+}
